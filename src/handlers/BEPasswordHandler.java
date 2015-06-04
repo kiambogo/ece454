6d2d5b32767d3d597d5192f6a9910bfe98b5c4f8
Anthony Clark <anthonyclark.ac@gmail.com>
2015-06-04 14:41:59 -0400

    BEPasswordHandler increments perf counters

--- src/handlers/BEPasswordHandler.java
+++ src/handlers/BEPasswordHandler.java
@@ -1,25 +1,33 @@
 package handlers;
 
-import org.apache.thrift.TException;
-
-import ece454.*;
-import services.*;
-
-import org.mindrot.jbcrypt.*;
+import ece454.A1Password;
+import ece454.ServiceUnavailableException;
+import org.mindrot.jbcrypt.BCrypt;
+import services.PerfCountersService;
 
 public class BEPasswordHandler implements A1Password.Iface {
     PerfCountersService countersService = new PerfCountersService();
 
     public String hashPassword(String password, short logRounds) throws ServiceUnavailableException, org.apache.thrift.TException {
+        countersService.incrementRequestsReceived();
+
         System.out.println("BE received request to hash. Hashing.");
         String hashed = BCrypt.hashpw(password, BCrypt.gensalt(logRounds));
         System.out.println("hashing...");
         while (hashed == null) { }
+
+        countersService.incrementRequestsCompleted();
       return hashed;
     }
 
     public boolean checkPassword(String password, String hash) throws org.apache.thrift.TException {
-        return BCrypt.checkpw(password, hash) ? true : false;
+        countersService.incrementRequestsReceived();
+
+        boolean check = BCrypt.checkpw(password, hash);
+
+        countersService.incrementRequestsCompleted();
+        return check;
+
     }
 }
 
