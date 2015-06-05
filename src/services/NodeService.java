cdae8de12d84b491b75466d42dcb3e2db1e8df09
Anthony Clark <anthonyclark.ac@gmail.com>
2015-06-04 22:40:23 -0400

    Weighted random BE server selection function added

--- src/services/NodeService.java
+++ src/services/NodeService.java
@@ -1,15 +1,15 @@
 package services;
 
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import ece454.*;
 import java.time.LocalTime;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class NodeService {
     public static ConcurrentHashMap<LocalTime, Heartbeat> BEMap = new ConcurrentHashMap<LocalTime, Heartbeat>(); 
     public static List<Node> seedList = new ArrayList<Node>();
+    private static Random random = new Random();
 
     public Set<Heartbeat> getListOfBENodes() {
         Set<Heartbeat> nodeSet = new HashSet<Heartbeat>(BEMap.values()); 
@@ -30,6 +30,27 @@ public class NodeService {
           if (timestamp.isBefore(currentTime.minusSeconds(1)))
             BEMap.remove(timestamp);
         }
+    }
+
+    public Heartbeat getBE(){
+        // BE hashmap to list
+        List<Heartbeat> BEArrayList = Arrays.asList( BEMap.values().toArray(new Heartbeat[0]) );
+        int total_cores = 0;
+        int counter;
+
+        // Calculate total cores
+        for(Heartbeat hb : BEArrayList){
+            total_cores += hb.numberOfCores;
+        }
+        counter = (int)(random.nextDouble() * total_cores );
+
+        for(Heartbeat hb : BEArrayList){
+            counter -= hb.numberOfCores;
+            if(counter <= 0){
+                return hb;
+            }
+        }
+        return null;
     }
 
 
