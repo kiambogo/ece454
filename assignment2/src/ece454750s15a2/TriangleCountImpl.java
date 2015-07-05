f2c9da8f1ca45298057ac22b3ffc6663d45de0f5
Anthony Clark <anthonyclark.ac@gmail.com>
2015-07-05 18:58:12 -0400

    Changed to use threadpools

--- assignment2/src/ece454750s15a2/TriangleCountImpl.java
+++ assignment2/src/ece454750s15a2/TriangleCountImpl.java
@@ -13,13 +13,13 @@ import java.util.concurrent.*;
 
 public class TriangleCountImpl {
   private byte[] input;
-  private int numCores;
-  private CopyOnWriteArrayList<Triangle> triangleList;
+  private int ncores;
+  private CopyOnWriteArrayList<Triangle> triangles;
 
-  public TriangleCountImpl(byte[] input, int numCores) {
+  public TriangleCountImpl(byte[] input, int ncores) {
     this.input = input;
-    this.numCores = numCores;
-    this.triangleList= new CopyOnWriteArrayList<Triangle>();
+    this.ncores = ncores;
+    this.triangles= new CopyOnWriteArrayList<Triangle>();
   }
 
   public List<String> getGroupMembers() {
@@ -31,38 +31,46 @@ public class TriangleCountImpl {
   }
 
   public List<Triangle> enumerateTriangles() throws IOException {
-    ArrayList<HashSet<Integer>> adjacencyList = getAdjacencyList(input);
-    if (numCores == 1) {
-      return triangleCount(0, 1, adjacencyList);
+    ArrayList<HashSet<Integer>> adjacencyLists = getAdjacencyList(input);
+    if (ncores == 1) {
+      return triangleCount(0, 1, adjacencyLists);
     } else {
-      CountThread[] threads = new CountThread[numCores];
-      ArrayList<Integer> ret = new ArrayList<Integer>();
-      for(int i = 0; i < numCores; i++) {
-        threads[i] = new CountThread(i, numCores, adjacencyList);
-        threads[i].start();
-        ret.add(1);
-      }
-      while(ret.contains(1)) {
-        for(int i = 0; i < numCores; i++){
-          if (!threads[i].isAlive()) {
-            ret.set(i, 0);
-          }
+
+
+      try{
+        ExecutorService executor = Executors.newFixedThreadPool(ncores);
+        for(int i=0; i<ncores; i++){
+          Runnable worker = new CounterThread(i, ncores, adjacencyLists);
+          executor.execute(worker);
         }
+
+        executor.shutdown();
+        executor.awaitTermination(100000l,TimeUnit.SECONDS);
       }
-      return triangleList;
+      catch(Exception ex){}
+
+      return triangles;
     }
   }
 
-  public List<Triangle> triangleCount(int threadNum, int totalThreads, ArrayList<HashSet<Integer>> adjacencyList) {
-    // triangle counting algorithm
+  public List<Triangle> triangleCount(int thread_id, int thread_count, ArrayList<HashSet<Integer>> adjacencyLists) {
+    // triangle counting
     ArrayList<Triangle> ret = new ArrayList<Triangle>();
-    int numVertices = adjacencyList.size();
-    for (int i = threadNum; i < numVertices; i+=totalThreads) {
-      for (Integer neighbour_j : adjacencyList.get(i)) {
-        for (Integer neighbour_k : adjacencyList.get(i)) {
-          if (neighbour_j < neighbour_k) {
-            if (adjacencyList.get(neighbour_j).contains(neighbour_k)) {
-              ret.add(new Triangle(i, neighbour_j, neighbour_k));
+    int vertice_count = adjacencyLists.size();
+    HashSet<Integer> neighbours = new HashSet<Integer>();
+
+    for (int i = thread_id; i < vertice_count; i += thread_count) {
+      neighbours = adjacencyLists.get(i);
+
+      for (int j : neighbours) { // all neighbours j > i
+        for (int k : neighbours) {
+
+          if (j < k) {
+
+            if (adjacencyLists.get(j).contains(k)) {
+
+              // i < j < k
+              ret.add(new Triangle(i, j, k));
             }
           }
         }
@@ -71,20 +79,21 @@ public class TriangleCountImpl {
     return ret;
   }
 
-  class CountThread extends Thread {
-    private int threadNum;
-    private int totalThreads;
-    private ArrayList<HashSet<Integer>> adjacencyList;
+  class CounterThread implements Runnable {
+    private final int id;
+    private final int thread_count;
+    private ArrayList<HashSet<Integer>> adjacencyLists;
 
-    public CountThread(int threadNum, int totalThreads, ArrayList<HashSet<Integer>> adjacencyList) {
-      this.threadNum = threadNum;
-      this.totalThreads = totalThreads;
-      this.adjacencyList = adjacencyList;
+    public CounterThread(int id, int thread_count, ArrayList<HashSet<Integer>> adjacencyLists){
+      this.id = id;
+      this.thread_count = thread_count;
+      this.adjacencyLists = adjacencyLists;
+
     }
 
     @Override
     public void run() {
-      triangleList.addAll(triangleCount(threadNum, totalThreads, adjacencyList));
+      triangles.addAll(triangleCount(id, thread_count, adjacencyLists));
       return;
     }
   }
@@ -108,13 +117,18 @@ public class TriangleCountImpl {
     }
     while ((strLine = br.readLine()) != null && !strLine.equals(""))   {
       parts = strLine.split(": ");
-      int vertex = Integer.parseInt(parts[0]);
+
+      int i = Integer.parseInt(parts[0]);
+
       if (parts.length > 1) {
         parts = parts[1].split(" +");
         for (String part: parts) {
-          int num = Integer.parseInt(part);
-          if (num > vertex) {
-            adjacencyList.get(vertex).add(num);
+
+          int j = Integer.parseInt(part);
+
+          // Only include neighbouts greater than i
+          if (j > i) {
+            adjacencyList.get(i).add(j);
           }
         }
       }
