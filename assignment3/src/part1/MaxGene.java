6c60b70bd30249a284e3047a8f21985038ba1a49
Christopher Poenaru <kiambogo@gmail.com>
2015-07-22 22:41:55 -0400

    part 1 done

--- assignment3/src/part1/MaxGene.java
+++ assignment3/src/part1/MaxGene.java
@@ -1,39 +1,62 @@
-package ece454.part1;
-
 import java.io.*;
-import org.apache.hadoop.util.*;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 public class MaxGene {
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(WordCount.class);
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = Job.getInstance(conf, "max gene");
+    job.setJarByClass(MaxGene.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
-
-
-    String inputFolder = args[0];
-    String outputFolder = args[1];
-
-    System.out.println("Input folder: " + inputFolder);
-    System.out.println("Output folder: " + outputFolder);
-
-    // read graph data from input file to main memory
-    File infile = new File(inputFolder);
-    InputStream in = new FileInputStream(infile);
-    byte[] data = new byte[(int)infile.length()];
-    in.read(data);
-    in.close();
   }
 
-  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
+  public static class TokenizerMapper 
+       extends Mapper<Object, Text, Text, Text>{
+    
+    private Text sample = new Text();
+      
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+      String max = ""; 
+      double maxVal = 0;
+      Integer counter = 0;
+      while (itr.hasMoreTokens()) {
+        if (counter == 0) {
+          sample.set(itr.nextToken());
+          counter++;
+        } else {
+          double val = Double.parseDouble(itr.nextToken());
+          if (val > maxVal) {
+            maxVal = val;
+            max = "gene_" + Integer.toString(counter);
+          } else if (val == maxVal) {
+            max += ",gene_" + Integer.toString(counter);
+          }
+          counter++;
+        }
+      }
+      context.write(sample, new Text(max));
+    }
   }
-
 }
