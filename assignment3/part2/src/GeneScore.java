ba0f74b3860d3dea532374e0e4b68b3d390fbe69
Anthony Clark <anthonyclark.ac@gmail.com>
2015-07-27 14:52:57 -0400

    Changing output type

--- assignment3/part2/src/GeneScore.java
+++ assignment3/part2/src/GeneScore.java
@@ -26,7 +26,7 @@ public class GeneScore {
     job.setMapperClass(TokenizerMapper.class);
     job.setReducerClass(GeneScoreClass.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
+    job.setOutputValueClass(DoubleWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
